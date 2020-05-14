660254cd006345d2b6295c9e260b2c9d94c271d6
hmiche <63804608+hmiche@users.noreply.github.com>
2020-05-14 04:42:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ import traceback
 import pandas as pd
 import numpy as np
 
+lr = joblib.load("model.pkl") # Load "model.pkl
+print ('Model loaded')
+model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+print ('Model columns loaded')
+
 # Your API definition
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
     if lr:
         try:
@@ -37,11 +42,7 @@ if __name__ == '__main__':
     try:
         port = int(sys.argv[1]) # This is for a command-line input
     except:
-        port = 12345 # If you don't provide any port the port will be set to 12345
+        port = 5000 # If you don't provide any port the port will be set to 12345
 
-    lr = joblib.load("model.pkl") # Load "model.pkl"
-    print ('Model loaded')
-    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
-    print ('Model columns loaded')
 
     app.run(port=port, debug=True)
